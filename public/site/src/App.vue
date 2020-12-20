c3ca9f6badea96d95a30460122036fb667e3ddfd
mkretzmann <mauriciohk@gmail.com>
2020-12-20 12:00:47 -0300

    Iniciado projeto vue

---
+++ public/site/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h1>Dev test</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+        v-if="user"
+      >
+        <span class="mr-2">{{user.name}}</span>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Home/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    user: null
+  }),
+};
+</script>
